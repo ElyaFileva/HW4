527c97a88f54ba81368fcfaff444b388a09a20a7
Elya Fileva <filevaelya@mail.ru>
2023-02-21 23:39:14 +0300

    Добавлена задача 4-1

---
+++ 4-1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {a} в степени {b} = {DegreeBofNumberA(a, b)}");
+
+int DegreeBofNumberA(int number, int degree)
+{
+  int result = 1;
+  for (int i = 1; i <= degree; i++)
+  {
+    result = result * number;
+  }
+  return result;
+}
