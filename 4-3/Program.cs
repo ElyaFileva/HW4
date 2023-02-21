8c37cb80beb3134ba96cfeb18de775ee2e8c482c
Elya Fileva <filevaelya@mail.ru>
2023-02-21 23:42:08 +0300

    Добавлена задача 4-3

---
+++ 4-3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] array = new int[8];
+PrintArray(array);
+void PrintArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  while (index < length)
+  {
+    collection[index] = new Random().Next(1, 100);
+    Console.Write(collection[index] + " ");
+    index++;
+  }
+}
