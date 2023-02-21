001ecb23e81c7986627f62e0a004117bd84c65d7
Elya Fileva <filevaelya@mail.ru>
2023-02-21 23:40:18 +0300

    Добавлена задача 4-2

---
+++ 4-2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел из числа {number} = {SumOfNumbersFromNumber(number)}");
+
+int SumOfNumbersFromNumber(int num)
+{
+  int sum = 0;
+  while (num != 0)
+  {
+    sum += num % 10;
+    num /= 10;
+  }
+  return sum;
+}
